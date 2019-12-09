3f1da5d6f07bfd6dd8ef45eb5d44900cbeffb68d
@danial.kalhori <danial.kalhori@gmail.com>
2019-12-09 22:01:29 +0330

    Words.

---
+++ PlanBox.Words/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlanBox.Words
+{
+    class Program
+    {
+        static List<Sentence> Sentences;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Write a sentence :");
+
+                string sentenceInput = Console.ReadLine();
+
+                Sentence sentence = new Sentence(sentenceInput);
+
+                if (!sentence.IsValid)
+                {
+                    Console.WriteLine("Please enter a sentence with more than one word.");
+                }
+                else
+                {
+                    SaveSentence(sentence);
+
+                    foreach (var row in sentence.Words)
+                    {
+                        Console.WriteLine(row.Key + ": " + row.Value);
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void SaveSentence(Sentence sentence)
+        {
+            if (Sentences == null)
+                Sentences = new List<Sentence>();
+
+            Sentences.Add(sentence);
+        }
+
+    }
+}
---
+++ PlanBox.Words/Sentence.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PlanBox.Words
+{
+    public class Sentence
+    {
+        string _sentence = string.Empty;
+        public Sentence(string sentence)
+        {
+            _sentence = sentence;
+            Words = CountAndSort();
+        }
+
+        public bool IsValid => GetWords().Count >= 2;
+            
+
+        public SortedDictionary<string,int> Words { get; private set; }
+
+        private IList<string> GetWords()
+        {
+            if (string.IsNullOrEmpty(_sentence))
+                return new List<string>();
+
+            return _sentence.Split(" ")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => s.First().ToString().ToUpper() + s.ToLower().Substring(1))
+                .ToList();
+        }
+
+        private SortedDictionary<string, int> CountAndSort()
+        {
+            var list = GetWords();
+
+            SortedDictionary<string, int> dictionary = new SortedDictionary<string, int>();
+
+            foreach (string word in list.Distinct())
+            {
+                dictionary.Add(word, list.Count(w => w == word));
+            }
+
+            return dictionary;
+        }
+    }
+}
